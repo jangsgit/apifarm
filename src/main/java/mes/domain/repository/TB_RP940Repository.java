31e2c99af7eee03dad44c54a8d4af5d999252d95
Kim-Hyeon-A <hyeona1026@cu.ac.kr>
2024-07-02 14:57:55 +0900

    TB_RP940Repository commit

--- src/main/java/mes/domain/repository/TB_RP940Repository.java
+++ src/main/java/mes/domain/repository/TB_RP940Repository.java
@@ -6,5 +6,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.Optional;
 
 public interface TB_RP940Repository extends JpaRepository<TB_RP940, String> {
+  Optional<TB_RP940> findById(String id);
+
   Optional<TB_RP940> findByUserid(String userid);
 }
