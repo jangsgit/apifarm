7248ec63e0f8a27e3759a5b6e686367ebd35c68c
woochul jung <ncdncjf@naver.com>
2024-09-05 09:38:42 +0900

    정우철_로봇 통계 현황

--- src/main/resources/static/js/ui.js
+++ src/main/resources/static/js/ui.js
@@ -88,6 +88,7 @@ $(document).ready(function () {
         "/gui/wm_gasbill_list/default",
         "/gui/wm_fuel_management/default",
         "/gui/wm_gasele_analysis/default",
+        "/gui/wm_robot_analysis/default",
 
         // 추가 예외 URL을 여기에 추가할 수 있습니다.
         "/gui/another_exception"
