35a57132a90cd9ad56fb9a352d303dfe9e90e082
Kim-Hyeon-A <hyeona1026@cu.ac.kr>
2024-09-13 10:42:52 +0900

    240913 html수정 / 도시가스청구서 업로드 기능

--- src/main/java/mes/app/cost/service/GasBillService.java
+++ src/main/java/mes/app/cost/service/GasBillService.java
@@ -1,12 +1,25 @@
 package mes.app.cost.service;
 
 import mes.domain.services.SqlRunner;
+import org.apache.pdfbox.pdmodel.PDDocument;
+import org.apache.pdfbox.rendering.PDFRenderer;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.core.io.FileSystemResource;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
 import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestTemplate;
 
+import javax.imageio.ImageIO;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
 
 @Service
 public class GasBillService {
@@ -14,20 +27,23 @@ public class GasBillService {
     @Autowired
     SqlRunner sqlRunner;
 
-    public List<Map<String, Object>> getMonthlyUsageSummary(String startDate, String endDate) {
+    public List<Map<String, Object>> getMonthlyUsageSummary(String year) {
 
         MapSqlParameterSource params = new MapSqlParameterSource();
+        params.addValue("year", year);
 
         String sql = """
                 SELECT
-                    DATEPART(month, INDATE) AS month,
-                    SUM(SMUSEQTY) AS total_gas_usage,
-                    SUM(SMUSEQTY) AS total_monthly_usage,
-                    SUM(ASKAMT) AS total_billing_amount
+                    SUBSTRING(STANDYM, 5, 2) AS month,
+                    SUM(SMUSEQTY) AS smuseqty,
+                    SUM(SMUSEHQTY) AS smusehqty,
+                    SUM(ASKAMT) AS askamt
                 FROM
                     TB_RP410
+                WHERE
+                    SUBSTRING(STANDYM, 1, 4) = :year
                 GROUP BY
-                    DATEPART(month, INDATE)
+                    SUBSTRING(STANDYM, 5, 2)
                 ORDER BY
                     month;
                 """;
@@ -35,5 +51,22 @@ public class GasBillService {
         List<Map<String, Object>> items = this.sqlRunner.getRows(sql, params);
         return items;
     }
+
+
+    public List<String> getYear() {
+        MapSqlParameterSource params = new MapSqlParameterSource();
+        String sql = """
+                SELECT DISTINCT SUBSTRING(STANDYM, 1, 4) AS year
+                FROM TB_RP410
+                ORDER BY year DESC;
+                """;
+
+        List<Map<String, Object>> rows = this.sqlRunner.getRows(sql, params);
+        List<String> years = rows.stream()
+                .map(row -> (String) row.get("year"))
+                .collect(Collectors.toList());
+        return years;
+    }
+
 }
 
--- src/main/java/mes/domain/repository/GasBillRepository.java
+++ src/main/java/mes/domain/repository/GasBillRepository.java
@@ -1,11 +1,7 @@
 package mes.domain.repository;
 
-import mes.domain.entity.TB_RP410;
-import org.springframework.data.domain.Sort;
+import mes.domain.entity.actasEntity.TB_RP410;
 import org.springframework.data.jpa.repository.JpaRepository;
-
-
-import java.util.List;
 
 public interface GasBillRepository extends JpaRepository<TB_RP410,String> {
 //    List<TB_RP410> findAll(Sort sort);
