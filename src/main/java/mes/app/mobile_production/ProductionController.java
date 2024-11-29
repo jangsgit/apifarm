42c37542a7f759f990ef07ccaea0ef1cb634ee57
LeeJaeWook <ljw960214@naver.com>
2024-11-29 10:22:24 +0900

    241129_이재욱_생산현황

--- src/main/java/mes/app/mobile_production/ProductionController.java
+++ src/main/java/mes/app/mobile_production/ProductionController.java
@@ -1,6 +1,8 @@
 package mes.app.mobile_production;
 
 import mes.app.mobile_production.service.ProductionService;
+import mes.domain.entity.User;
+import mes.domain.entity.actasEntity.TB_DA006W_PK;
 import mes.domain.model.AjaxResult;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
@@ -9,6 +11,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -18,23 +21,165 @@ import java.util.Map;
 public class ProductionController {
     @Autowired
     private ProductionService productionService;
-
+    // 프로시저 리스트
     @GetMapping("/read_all")
     public AjaxResult productionList(@RequestParam(value = "search_startDate", required = false) String searchStartDate,
                                      @RequestParam(value = "search_endDate", required = false) String searchEndDate,
-                                     @RequestParam(value = "search_product", required = false) String searchRemark,
-                                     @RequestParam(value = "search_cltcd", required = false) String searchOrdfalg,
+                                     @RequestParam(value = "search_product", required = false) String searchProduct,
+                                     @RequestParam(value = "search_cltcd", required = false) String searchCltcd,
                                      Authentication auth) {
+        User user = (User) auth.getPrincipal();
+        String username = user.getUsername();
+//        Map<String, Object> userInfo = productionService.getUserInfo(username);
+        String search_startDate = (searchStartDate).replaceAll("-","");
+        String search_endDate = (searchEndDate).replaceAll("-","");
+//        임시 spjangcd / custcd
+        String spjangcd = "ZZ";
+        String custcd = "GRACE";
 
         Map<String, Object> searchLabels = new HashMap<>();
         searchLabels.put("search_startDate", searchStartDate);
         searchLabels.put("search_endDate", searchEndDate);
-        searchLabels.put("search_product", searchOrdfalg);
-        searchLabels.put("search_cltcd", searchOrdfalg);
+//        searchLabels.put("search_spjangcd", (String) userInfo.get("spjangcd"));
+//        searchLabels.put("search_custcd", (String) userInfo.get("custcd"));
+        searchLabels.put("search_spjangcd", spjangcd);
+        searchLabels.put("search_custcd", custcd);
+        searchLabels.put("search_product", searchProduct);
+        searchLabels.put("search_cltcd", searchCltcd);
         List<Map<String, Object>> productList = productionService.getProductionList(searchLabels);
+
+        productList.forEach(product -> {
+            // 기존 값을 가져오기
+            if(product.get("WFLAG01") != null) {
+                String originalValue1 = product.get("WFLAG01").toString();
+                if (originalValue1.length() > 2) {
+                    originalValue1 = originalValue1.substring(2);
+                    // 새로운 값으로 업데이트
+                    Map<String, Object> newValue = productionService.getProcess(originalValue1);
+                    // 맵에 업데이트된 값 넣기
+                    product.put("WFLAG01", newValue.get("com_cnam"));
+                }
+            }
+
+            if(product.get("WFLAG02") != null) {
+                String originalValue2 = product.get("WFLAG02").toString();
+                if (originalValue2.length() > 2) {
+                    originalValue2 = originalValue2.substring(2);
+                    Map<String, Object> newValue2 = productionService.getProcess(originalValue2);
+                    product.put("WFLAG02", newValue2.get("com_cnam"));
+                }
+            }
+            if(product.get("WFLAG03") != null) {
+                String originalValue3 = product.get("WFLAG03").toString();
+                if (originalValue3.length() > 2) {
+                    originalValue3 = originalValue3.substring(2);
+                    Map<String, Object> newValue3 = productionService.getProcess(originalValue3);
+                    product.put("WFLAG03", newValue3.get("com_cnam"));
+                }
+            }
+            // 날짜 형식 변환 (PRODDATE)
+            if (product.containsKey("PRODDATE")) {
+                String setupdt = (String) product.get("PRODDATE");
+                if (setupdt != null && setupdt.length() == 8) {
+                    String formattedDate = setupdt.substring(0, 4) + "-" + setupdt.substring(4, 6) + "-" + setupdt.substring(6, 8);
+                    product.put("PRODDATE", formattedDate);
+                }
+            }
+            // 날짜 형식 변환 (WORDT01)
+            if (product.containsKey("WORDT01")) {
+                String setupdt = (String) product.get("WORDT01");
+                if (setupdt != null && setupdt.length() == 8) {
+                    String formattedDate = setupdt.substring(0, 4) + "-" + setupdt.substring(4, 6) + "-" + setupdt.substring(6, 8);
+                    product.put("WORDT01", formattedDate);
+                }
+            }
+            // 날짜 형식 변환 (WORDT02)
+            if (product.containsKey("WORDT02")) {
+                String setupdt = (String) product.get("WORDT02");
+                if (setupdt != null && setupdt.length() == 8) {
+                    String formattedDate = setupdt.substring(0, 4) + "-" + setupdt.substring(4, 6) + "-" + setupdt.substring(6, 8);
+                    product.put("WORDT02", formattedDate);
+                }
+            }
+            // 날짜 형식 변환 (WORDT03)
+            if (product.containsKey("WORDT03")) {
+                String setupdt = (String) product.get("WORDT03");
+                if (setupdt != null && setupdt.length() == 8) {
+                    String formattedDate = setupdt.substring(0, 4) + "-" + setupdt.substring(4, 6) + "-" + setupdt.substring(6, 8);
+                    product.put("WORDT03", formattedDate);
+                }
+            }
+        });
 
         AjaxResult result = new AjaxResult();
         result.data = productList;
         return result;
     }
+    // 작업이력 팝업 데이터
+    @GetMapping("/read_work")
+    public AjaxResult workList(@RequestParam(value = "wono", required = false) String wono,
+                                     Authentication auth) {
+        User user = (User) auth.getPrincipal();
+        String username = user.getUsername();
+        Map<String, Object> userInfo = productionService.getUserInfo(username);
+
+        Map<String, Object> searchLabels = new HashMap<>();
+//        searchLabels.put("search_spjangcd", (String) userInfo.get("spjangcd"));
+//        searchLabels.put("search_custcd", (String) userInfo.get("custcd"));
+        String spjangcd = "ZZ";
+        searchLabels.put("search_spjangcd", spjangcd);
+        String custcd = "GRACE";
+        searchLabels.put("search_custcd", custcd);
+
+        searchLabels.put("wono", wono);
+        List<Map<String, Object>> productList = productionService.getWorkList(searchLabels);
+        productList.forEach(product -> {
+            // 기존 값을 가져오기
+            if(product.get("wflag") != null) {
+                String originalValue1 = product.get("wflag").toString();
+                if (originalValue1.length() > 2) {
+                    originalValue1 = originalValue1.substring(2);
+                    // 새로운 값으로 업데이트
+                    Map<String, Object> newValue = productionService.getProcess(originalValue1);
+                    // 맵에 업데이트된 값 넣기
+                    product.put("wflag", newValue.get("com_cnam"));
+                }
+            }
+
+            // 날짜 형식 변환 (wtrdt)
+            if (product.containsKey("wtrdt")) {
+                String setupdt = (String) product.get("wtrdt");
+                if (setupdt != null && setupdt.length() == 8) {
+                    String formattedDate = setupdt.substring(0, 4) + "-" + setupdt.substring(4, 6) + "-" + setupdt.substring(6, 8);
+                    product.put("wtrdt", formattedDate);
+                }
+            }
+        });
+        AjaxResult result = new AjaxResult();
+        result.data = productList;
+        return result;
+    }
+    // 거래처 검색
+    @GetMapping("/search_cltcd")
+    public AjaxResult searchCltcd(@RequestParam(value = "search_cltnm", required = false) String search_cltnm,
+                               Authentication auth) {
+
+        List<Map<String, Object>> productList = productionService.searchCltcd(search_cltnm);
+
+        AjaxResult result = new AjaxResult();
+        result.data = productList;
+        return result;
+    }
+    // 품목 검색
+    @GetMapping("/search_product")
+    public AjaxResult searchProduct(@RequestParam(value = "search_productnm", required = false) String search_product,
+                               Authentication auth) {
+
+        List<Map<String, Object>> productList = productionService.searchProduct(search_product);
+
+        AjaxResult result = new AjaxResult();
+        result.data = productList;
+        return result;
+    }
+
 }
--- src/main/java/mes/app/mobile_production/service/ProductionService.java
+++ src/main/java/mes/app/mobile_production/service/ProductionService.java
@@ -1,6 +1,7 @@
 package mes.app.mobile_production.service;
 
 import mes.domain.entity.actasEntity.TB_DA006W_PK;
+import mes.domain.model.AjaxResult;
 import mes.domain.services.SqlRunner;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
@@ -8,6 +9,7 @@ import org.springframework.stereotype.Service;
 
 import java.sql.Types;
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
@@ -15,6 +17,22 @@ import java.util.Map;
 public class ProductionService {
     @Autowired
     SqlRunner sqlRunner;
+
+    // username으로 cltcd, cltnm, saupnum, custcd 가지고 오기
+    public Map<String, Object> getUserInfo(String username) {
+        MapSqlParameterSource dicParam = new MapSqlParameterSource();
+
+        String sql = """
+                select xs.custcd,
+                       au.spjangcd
+                FROM TB_XUSERS xs
+                left join auth_user au on au."username" = xs.userid
+                WHERE xs.userid = :username
+                """;
+        dicParam.addValue("username", username);
+        Map<String, Object> userInfo = this.sqlRunner.getRow(sql, dicParam);
+        return userInfo;
+    }
 
     //카드리스트 불러오기
     public List<Map<String, Object>> getProductionList(Map<String, Object> searchLabels) {
@@ -44,6 +62,114 @@ public class ProductionService {
             e.printStackTrace();
         }
         return items != null ? items : List.of();
+    }
+
+    //작지리스트 불러오기
+    public List<Map<String, Object>> getWorkList(Map<String, Object> searchLabels) {
+
+        MapSqlParameterSource dicParam = new MapSqlParameterSource();
+        List<Map<String, Object>> items = new ArrayList<>();
+        String sql = "  SELECT CONVERT(CHAR(8), TB_FPLAN_WORK.wtrdt, 112) AS wtrdt,   \n" +
+                "       TB_FPLAN.plan_no,   \n" +
+                "       TB_FPLAN.wono,   \n" +
+                "       TB_FPLAN.pcode,   \n" +
+                "       TB_CA501.phm_pnam AS pname,   \n" +
+                "       TB_CA501.phm_size AS psize,   \n" +
+                "       TB_CA501.phm_unit AS punit,   \n" +
+                "       TB_FPLAN.cltcd,\n" +
+                "       TB_FPLAN_WORK.wflag AS wflag,\n" +
+                "       TB_FPLAN_WORK.wotqt AS wotqt,   \n" +
+                "       TB_FPLAN_WORK.wbdqt AS wbdqt,   \n" +
+                "       TB_FPLAN_WORK.wotqt - TB_FPLAN_WORK.wbdqt AS wokqt,   \n" +
+                "       TB_FPLAN_WORK.wremark AS remark\n" +
+                "FROM TB_FPLAN WITH(NOLOCK)\n" +
+                "INNER JOIN TB_FPLAN_WORK WITH(NOLOCK) ON \n" +
+                "       TB_FPLAN.custcd = TB_FPLAN_WORK.custcd AND\n" +
+                "       TB_FPLAN.spjangcd = TB_FPLAN_WORK.spjangcd AND\n" +
+                "       TB_FPLAN.plan_no = TB_FPLAN_WORK.plan_no\n" +
+                "INNER JOIN TB_CA501 WITH(NOLOCK) ON \n" +
+                "       TB_FPLAN.custcd = TB_CA501.phm_cust AND\n" +
+                "       TB_FPLAN.pcode = TB_CA501.phm_pcod\n" +
+                "WHERE TB_FPLAN.custcd = :custcd\n" +
+                "  AND TB_FPLAN.spjangcd = :spjangcd\n" +
+                "  AND TB_FPLAN.wono = :wono\n" +
+                "  AND TB_FPLAN_WORK.decision = '0';";
+
+        dicParam.addValue("custcd", searchLabels.get("search_custcd"));
+        dicParam.addValue("spjangcd", searchLabels.get("search_spjangcd"));
+        dicParam.addValue("wono", searchLabels.get("wono"));
+
+        try {
+            items = this.sqlRunner.getRows(sql, dicParam);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return items != null ? items : List.of();
+    }
+
+    //거래처 검색
+    public List<Map<String, Object>> searchCltcd (String searchCltnm) {
+        MapSqlParameterSource dicParam = new MapSqlParameterSource();
+        List<Map<String, Object>> items = new ArrayList<>();
+        String sql = "SELECT cltcd,   " + // 거래처 코드
+                "       cltnm     " +   // 거래처명
+                "  FROM TB_XCLIENT WITH(NOLOCK)" +
+                " WHERE (cltcd  LIKE :searchCltcd" +
+                "    OR  cltnm  LIKE :searchCltcd)";
+
+        if(!searchCltnm.isEmpty()) {
+            dicParam.addValue("searchCltcd", "%" + searchCltnm + "%");  // searchLabels.get("param1")
+        }else {
+            dicParam.addValue("searchCltcd", "%");
+        }
+        try {
+            items = this.sqlRunner.getRows(sql, dicParam);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return items != null ? items : List.of();
+    }
+
+    // 품목 검색
+    public List<Map<String, Object>> searchProduct(String searchProduct) {
+        MapSqlParameterSource dicParam = new MapSqlParameterSource();
+        List<Map<String, Object>> items = new ArrayList<>();
+        String sql = "SELECT   TB_CA501.phm_pcod," + // 제품코드
+                "       TB_CA501.phm_pnam, " + // 품명
+                "       TB_CA501.phm_size   FROM TB_CA501 WITH(NOLOCK)" + // 규격
+                " WHERE (TB_CA501.phm_pcod LIKE :searchProduct " +
+                "    OR  TB_CA501.phm_pnam LIKE :searchProduct " +
+                "    OR  TB_CA501.phm_size LIKE :searchProduct ) " +
+                "   AND (TB_CA501.useyn    = '1')";
+
+        if(!searchProduct.isEmpty()) {
+            dicParam.addValue("searchProduct","%" + searchProduct + "%");
+        }else {
+            dicParam.addValue("searchProduct", "%");
+        }
+
+
+        try {
+            items = this.sqlRunner.getRows(sql, dicParam);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return items != null ? items : List.of();
+    }
+
+    // wflag 매핑
+    public Map<String, Object> getProcess(String comCode) {
+        Map<String, Object> item = new HashMap<>();
+        MapSqlParameterSource dicParam = new MapSqlParameterSource();
+        String sql = "select com_cnam from TB_CA510 where com_cls = '040' and com_code<>'00' AND com_code = :com_code";
+
+        dicParam.addValue("com_code", comCode);
+        try {
+            item = this.sqlRunner.getRow(sql, dicParam);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return item;
     }
 
 }
